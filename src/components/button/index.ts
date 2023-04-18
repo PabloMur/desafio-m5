ed865a5f0555c5b72409c39dd661e8c3a8e76b80
PabloMur <polillomurillo@gmail.com>
2023-04-17 21:56:56 -0300

    optimizacion con chat gpt

--- src/components/button/index.ts
+++ src/components/button/index.ts
@@ -1,4 +1,38 @@
 export function initCustomButton() {
+  const buttonTemplate = document.createElement("template");
+  buttonTemplate.innerHTML = `
+    <style>
+      .root{
+          font-size: 18px;
+          border-radius: 4px;
+          padding: 17px 13px;
+          background-color:#006CFC;
+          color:#D8FCFC;
+          width: 90vw;
+          border: 10px solid #001997;
+          animation: lower .4s ease ;
+      }
+      @media screen and (min-width: 960px){
+        .root{
+          max-width: 600px;
+          margin: 0 auto;
+        }
+      }
+      @keyframes lower{
+        0%{
+            transform: translateY(100%);
+        }
+        75%{
+            transform: translateY(-10%);
+        }
+        100%{
+            transform: translateY(0%);
+        }
+      }
+    </style>
+    <button class="root"><slot>ups!</slot></button>
+  `;
+
   customElements.define(
     "custom-button",
     class extends HTMLElement {
@@ -8,45 +42,7 @@ export function initCustomButton() {
         this.render();
       }
       render() {
-        const button = document.createElement("button");
-        const style = document.createElement("style");
-
-        button.className = "root";
-
-        style.innerHTML = `
-              .root{
-                  font-size: 18px;
-                  border-radius: 4px;
-                  padding: 17px 13px;
-                  background-color:#006CFC;
-                  color:#D8FCFC;
-                  width: 90vw;
-                  border: 10px solid #001997;
-                  animation: lower .4s ease ;
-              }
-              @media screen and (min-width: 960px){
-                .root{
-                  max-width: 600px;
-                  margin: 0 auto;
-                }
-              }
-              @keyframes lower{
-                0%{
-                    transform: translateY(100%);
-                }
-                75%{
-                    transform: translateY(-10%);
-                }
-                100%{
-                    transform: translateY(0%);
-                }
-            }
-            `;
-
-        button.textContent = this.textContent || "ups!";
-
-        this.shadow.appendChild(button);
-        this.shadow.appendChild(style);
+        this.shadow.appendChild(buttonTemplate.content.cloneNode(true));
       }
     }
   );
